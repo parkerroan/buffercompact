d67a8280927ba06b3e09755cfb73854f17de85a4
Parker Roan <parker@shipt.com>
2022-02-07 22:45:48 -0600

    ading example file

---
+++ examples/getting_started.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	badger "github.com/dgraph-io/badger/v3"
+	"github.com/parkerroan/buffercompact"
+	"github.com/parkerroan/buffercompact/sortedset"
+)
+
+func main() {
+	sortedset := sortedset.New()
+	db, _ := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+	bufferDuration := 1 * time.Second
+
+	buffcomp, _ := buffercompact.New(db, sortedset, bufferDuration, buffercompact.WithMaxValues(5))
+
+	buffcomp.StoreToQueue("test1", []byte("testValue1"))
+	buffcomp.StoreToQueue("test1", []byte("testValue2"))
+
+	items, _ := buffcomp.RetreiveFromQueue(10)
+	fmt.Printf("No Results: %v \n", items)
+
+	time.Sleep(1 * time.Second)
+
+	items, _ = buffcomp.RetreiveFromQueue(10)
+	fmt.Printf("Results After Buffer Duration: %v \n", items)
+
+	//Insert More Records Up To Max
+	buffcomp.StoreToQueue("test1", []byte("testValue1"))
+	buffcomp.StoreToQueue("test2", []byte("testValue2"))
+	buffcomp.StoreToQueue("test3", []byte("testValue3"))
+	buffcomp.StoreToQueue("test4", []byte("testValue4"))
+	buffcomp.StoreToQueue("test5", []byte("testValue5"))
+	buffcomp.StoreToQueue("test6", []byte("testValue6"))
+
+	items, _ = buffcomp.RetreiveFromQueue(10)
+	fmt.Printf("All Results Returned Due To Max Values Limit (Ignored Buffer Duration): %v \n", items)
+}
