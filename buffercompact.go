4ef6bd766bade600154966c6f6e29a1a042ca73c
Parker Roan <parker@shipt.com>
2022-02-07 21:58:57 -0600

    init

---
+++ buffercompact.go
@@ -0,0 +1,137 @@
+package buffercompact
+
+import (
+	"time"
+
+	badger "github.com/dgraph-io/badger/v3"
+	"github.com/parkerroan/buffercompact/sortedset"
+)
+
+type BufferCompactor struct {
+	db             *badger.DB
+	sortedSet      *sortedset.SortedSet
+	bufferDuration time.Duration
+}
+
+type StorageItem struct {
+	Key   string
+	Value []byte
+}
+
+func New(db *badger.DB, sortedSet *sortedset.SortedSet, bufferDuration time.Duration, opts ...func(*BufferCompactor)) (*BufferCompactor, error) {
+	byffComp := BufferCompactor{
+		db:             db,
+		sortedSet:      sortedSet,
+		bufferDuration: bufferDuration,
+	}
+
+	for _, opt := range opts {
+		opt(&byffComp)
+	}
+
+	return &byffComp, nil
+}
+
+func (b *BufferCompactor) StoreToQueue(key string, value []byte) error {
+
+	err := b.db.Update(func(txn *badger.Txn) error {
+		err := txn.Set([]byte(key), value)
+		return err
+	})
+	if err != nil {
+		return err
+	}
+
+	if node := b.sortedSet.GetByKey(key); node == nil {
+		score := sortedset.SCORE(time.Now().Add(b.bufferDuration).Unix())
+		b.sortedSet.AddOrUpdate(key, score, struct{}{})
+	}
+	return nil
+}
+
+func (b *BufferCompactor) RetreiveFromQueue(limit int) ([]*StorageItem, error) {
+
+	end := sortedset.SCORE(time.Now().Unix())
+	nodes := b.sortedSet.GetByScoreRange(-1, end, &sortedset.GetByScoreRangeOptions{
+		Limit:  limit,
+		Remove: true})
+
+	response := make([]*StorageItem, 0, len(nodes))
+
+	//TODO there is obvious performace improvement opportunity in a bulk transaction.
+	//but need to weigh the risk of the transaction errors by growing too large.
+	//https://dgraph.io/docs/badger/get-started/#read-write-transactions (check example here)
+	for i := range nodes {
+		item, err := b.RemoveFromDB(nodes[i].Key())
+
+		if err != nil {
+			return nil, err
+		}
+		response = append(response, item)
+	}
+
+	return response, nil
+}
+
+//RemoveFromDB reads and deletes by key from badger in a read-write transaction
+func (b *BufferCompactor) RemoveFromDB(key string) (*StorageItem, error) {
+	txn := b.db.NewTransaction(true)
+	defer txn.Discard()
+
+	item, err := txn.Get([]byte(key))
+	if err != nil {
+		return nil, err
+	}
+	if err := txn.Delete([]byte(key)); err != nil {
+		return nil, err
+	}
+
+	if err := txn.Commit(); err != nil {
+		return nil, err
+	}
+
+	var value []byte
+	value, err = item.ValueCopy(value)
+	if err != nil {
+		return nil, err
+	}
+
+	return &StorageItem{
+		Key:   key,
+		Value: value,
+	}, nil
+}
+
+//RetrieveAllKeys is a helper that can be used on startup if using badger with persistant disk options.
+//On startup the SortedSet will be empty since it will not persist to the disk; if persiting badger, you may
+//    use this function to drive iteration in order to reseed the SortedSet OR
+//    process manually and pop them from the store using `RemoveFromDB`
+//
+//Note: This is a helper function and can be implemented on the badger db instance itself and is not pivotal to this library.
+func (b *BufferCompactor) RetrieveAllKeys() ([]string, error) {
+	var keys []string
+	err := b.db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchSize = 10
+		it := txn.NewIterator(opts)
+		defer it.Close()
+		for it.Rewind(); it.Valid(); it.Next() {
+			item := it.Item()
+			k := item.Key()
+			err := item.Value(func(v []byte) error {
+				keys = append(keys, string(k))
+				return nil
+			})
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return keys, nil
+}
---
+++ buffercompact_test.go
@@ -0,0 +1,102 @@
+package buffercompact
+
+import (
+	"testing"
+	"time"
+
+	badger "github.com/dgraph-io/badger/v3"
+	"github.com/parkerroan/buffercompact/sortedset"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_New(t *testing.T) {
+	sortedset := sortedset.New()
+	db, _ := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+	bufferDuration := 5 * time.Second
+
+	buffcomp, err := New(db, sortedset, bufferDuration)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, buffcomp)
+}
+
+func Test_NormalUsage(t *testing.T) {
+	sortedset := sortedset.New()
+	db, _ := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+	bufferDuration := 1 * time.Second
+
+	buffcomp, err := New(db, sortedset, bufferDuration)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, buffcomp)
+
+	buffcomp.StoreToQueue("test1", []byte("testValue1"))
+	buffcomp.StoreToQueue("test1", []byte("testValue2"))
+	buffcomp.StoreToQueue("test2", []byte("testValue3"))
+
+	//No Wait
+	items, err := buffcomp.RetreiveFromQueue(10)
+	assert.Nil(t, err)
+	assert.Len(t, items, 0)
+
+	//items should compact and only receive 2
+	time.Sleep(2 * time.Second)
+	items, err = buffcomp.RetreiveFromQueue(10)
+	assert.Nil(t, err)
+	assert.Len(t, items, 2)
+
+	assert.Equal(t, "test1", string(items[0].Key))
+	assert.Equal(t, "testValue2", string(items[0].Value))
+	assert.Equal(t, "test2", string(items[1].Key))
+	assert.Equal(t, "testValue3", string(items[1].Value))
+}
+
+func Test_StoreToQueue(test *testing.T) {
+	cases := map[string]struct {
+		item        StorageItem
+		expectedErr error
+	}{
+		"Happy Path": {
+			item: StorageItem{
+				Key:   "test1",
+				Value: []byte("test_value1"),
+			},
+		},
+	}
+
+	for name, c := range cases {
+		test.Run(name, func(t *testing.T) {
+			sortedset := sortedset.New()
+			db, _ := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+			bufferDuration := 5 * time.Second
+
+			buffcomp, err := New(db, sortedset, bufferDuration)
+			assert.Nil(t, err)
+
+			err = buffcomp.StoreToQueue(c.item.Key, c.item.Value)
+			if c.expectedErr == nil {
+				assert.Nil(t, err)
+				node := sortedset.GetByKey(c.item.Key)
+				assert.Equal(t, c.item.Key, node.Key())
+
+				if err := db.View(func(txn *badger.Txn) error {
+					dbItem, err := txn.Get([]byte(c.item.Key))
+					if err != nil {
+						t.Fatal("error in bagder txn")
+					}
+
+					var dbValue []byte
+					dbValue, err = dbItem.ValueCopy(dbValue)
+
+					assert.Equal(t, c.item.Value, dbValue)
+					return err
+				}); err != nil {
+					t.Fatal("error in bagder txn")
+				}
+
+			} else {
+				assert.EqualError(t, err, c.expectedErr.Error())
+			}
+		})
+	}
+}
